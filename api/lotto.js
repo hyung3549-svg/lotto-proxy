fa7eea3170b9a1ebb7e0ca85e398afaf788b47ae
hyung3549-svg <hyung3549@gmail.com>
2026-04-05 16:48:33 +0900

    Update lotto.js

--- api/lotto.js
+++ api/lotto.js
@@ -1,5 +1,4 @@
 import lottoData from '../lotto.json' assert { type: 'json' };
-
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(lottoData);
